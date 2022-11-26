d20d3e0ef6b11cfaaa5254a5818d4b9f5bd16b3a
sszkarlat <sszkarlat@student.agh.edu.pl>
2022-11-26 11:09:03 +0100

    Wykonanie zadania 6 z zestawu 4. Program napisany w Python. Sprawdza czy podana przez użytkownika liczba jest pierwsza.

--- laboratorium25.10/liczbyPierwsze.py
+++ laboratorium25.10/liczbyPierwsze.py
@@ -2,11 +2,21 @@ def is_prime(number):
     if number < 2:
         return False
     else:
-        for divisor in range(2, number):
+        for divisor in range(2, int((number ** 0.5) + 1)):
             if number % divisor == 0:
                 return False
         return True
 
 
-request = int(input("Podaj liczbę, która chcesz sprawdzić: "))
-print(is_prime(request))
+"""Przykłady testowe:
+268979
+7
+811709
+925189
+1890571
+325826571
+17592186044415"""
+
+while True:
+    request = int(input("Podaj liczbę, która chcesz sprawdzić: "))
+    print(is_prime(request))
